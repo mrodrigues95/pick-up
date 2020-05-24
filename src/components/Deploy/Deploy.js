751debb05c5c9c9af0536f9c67b4512227dba653
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-24 15:54:06 -0400

    added a form component

--- src/components/Deploy/Deploy.js
+++ src/components/Deploy/Deploy.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Layout from '../Layout';
 import Navigation from '../Navigation/Navigation';
+import Form from '../UI/Form';
 
 const Deploy = () => {
   return (
     <Layout>
       <Navigation />
+      <Form />
     </Layout>
   );
 };
---
+++ src/components/UI/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+      <form className="bg-white border border-black border-opacity-0 shadow-md mt-2 mx-3">
+          <h1>Test</h1>
+      </form>
+  );
+};
+
+export default Form;
