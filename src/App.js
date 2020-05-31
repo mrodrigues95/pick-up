011d28feeb41a031b00bf31d637a03f1d0541d34
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-05-31 14:45:30 -0400

    added a 404 page not found component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './css/main.css';
 
 import Home from './components/Home/Home';
@@ -8,17 +8,21 @@ import Deploy from './components/Deploy/Deploy';
 import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
 import PrivateRoute from './components/Auth/PrivateRoute';
+import FourOhFour from './components/404/FourOhFour';
 import { AuthProvider } from './components/Auth/Auth';
 
 const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
-        <PrivateRoute exact path="/orders" component={Orders} />
-        <PrivateRoute exact path="/deploy" component={Deploy} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <PrivateRoute exact path="/orders" component={Orders} />
+          <PrivateRoute exact path="/deploy" component={Deploy} />
+          <Route path="*" component={FourOhFour} />
+        </Switch>
       </Router>
     </AuthProvider>
   );
---
+++ src/components/404/FourOhFour.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Layout from './../Layout';
+
+const FourOhFour = () => {
+  return (
+    <Layout>
+      <div className="mt-6 ml-6">
+        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
+          Page not found.
+        </h2>
+        <p className="my-4 text-gray-900">
+          The page you're looking could not be found.
+        </p>
+        <Link to="/">Go back home.</Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default FourOhFour;
